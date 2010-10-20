27d43c6ec39a8f0de3a9026caef9ea9330c9f2f3
Ralph Bean <ralph.bean@gmail.com>
2010-10-20 09:44:58 -0400

    spacing for readability

--- development-deps/develop-tw2-destroy-and-setup.sh
+++ development-deps/develop-tw2-destroy-and-setup.sh
@@ -21,6 +21,7 @@ hg clone http://bitbucket.org/paj/tw2devtools || \
         (pushd tw2devtools && hg pull && popd)
 hg clone http://bitbucket.org/paj/tw2forms || \
         (pushd tw2forms && hg pull && popd)
+
 hg clone http://bitbucket.org/toscawidgets/tw2jquery || \
         (pushd tw2jquery && hg pull && popd)
 git clone git://github.com/ralphbean/tw2.jquery.plugins.ui.git || \
@@ -29,8 +30,10 @@ git clone git://github.com/ralphbean/tw2.jquery.plugins.fg.git || \
         (pushd tw2.jquery.plugins.fg && git pull && popd)
 git clone git://github.com/ralphbean/tw2.jquery.plugins.jqgrid.git || \
         (pushd tw2.jquery.plugins.jqgrid && git pull && popd)
+
 git clone git://github.com/ralphbean/tw2.jit.git || \
         (pushd tw2.jit && git pull && popd)
+
 git clone git://github.com/ralphbean/tw2.protovis.core.git || \
         (pushd tw2.protovis.core && git pull && popd)
 git clone git://github.com/ralphbean/tw2.protovis.conventional.git || \
@@ -43,11 +46,14 @@ git clone git://github.com/ralphbean/tw2.protovis.hierarchies.git || \
 pushd tw2core ;  python setup.py develop ; popd
 pushd tw2forms ; python setup.py develop ; popd
 pushd tw2devtools ; python setup.py develop ; popd
+
 pushd tw2jquery ; python setup.py install_lib install_egg_info ; popd
 pushd tw2.jquery.plugins.ui ; python setup.py install_lib install_egg_info ; popd
 pushd tw2.jquery.plugins.fg ; python setup.py install_lib install_egg_info ; popd
 pushd tw2.jquery.plugins.jqgrid ; python setup.py install_lib install_egg_info ; popd
+
 pushd tw2.jit ; python setup.py develop ; popd
+
 pushd tw2.protovis.core ; python setup.py develop ; popd
 pushd tw2.protovis.conventional ; python setup.py develop ; popd
 pushd tw2.protovis.custom ; python setup.py develop ; popd
